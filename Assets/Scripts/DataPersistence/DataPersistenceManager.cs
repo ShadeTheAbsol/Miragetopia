e4c90d8402e16b247a8d31658cfd0c9e390ef2e8
GrantL <grantlittlefield2@gmail.com>
2024-05-21 01:14:36 +0200

    Created a DataPersistence folder and a DataPersistenceManager script for managing saving and loading of data between sessions.

---
+++ Assets/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+    public static DataPersistenceManager instance { get; private set;}
+
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObjects;
+
+    [Header("File Storage Config")]
+    [SerializeField] private string fileName;
+    private FileDataHandler dataHandler;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        this.dataPersistenceObjects = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        //Load Save Data from a file using the data handler
+        this.gameData = dataHandler.Load();
+
+        //if no data to load, initialize to new game
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults.");
+            NewGame();
+        }
+
+        //Push the loaded data to all scripts that need it.
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+
+        Debug.Log("Grid Size Loaded: Columns: " + gameData.columns + " Rows: " + gameData.rows);
+    }
+
+    public void SaveGame()
+    {
+        //Pass Save Data to other scripts so they can update it.
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.SaveData(gameData);
+        }
+        Debug.Log("Grid Size Saved: Columns: " + gameData.columns + " Rows: " + gameData.rows);
+        //Save the Save Data to a file using the data handler
+        dataHandler.Save(gameData);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if(focus == false)
+        SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistenceObjects);
+    }
+}

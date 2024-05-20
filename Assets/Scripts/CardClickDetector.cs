0f2d53995615f412058cc69d69cfc34e868cc213
GrantL <grantlittlefield2@gmail.com>
2024-05-20 12:28:02 +0200

    Created a script for detecting if a card is clicked and for playing various animations on a card based on current actions taken on the card such as revealing a card and checking cards for a matching pair.

---
+++ Assets/Scripts/CardClickDetector.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardClickDetector : MonoBehaviour
+{
+    Animator cardAnim;
+    BoxCollider2D cardCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardAnim = GetComponentInChildren<Animator>();
+        cardCol = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        ShowCard();
+        GameManager.instance.AssignCardsToCheck(gameObject);
+    }
+
+    public void ShowCard()
+    {
+        cardAnim.SetTrigger("ShowCard");
+        cardCol.enabled = false;
+    }
+
+    public void HideCard()
+    {
+        cardAnim.SetTrigger("HideCard");
+        cardCol.enabled = true;
+    }
+
+    public void CardMatch()
+    {
+        cardAnim.SetTrigger("CardMatch");
+    }
+}
